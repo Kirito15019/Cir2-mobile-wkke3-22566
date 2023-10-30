2dfe4bd5bf1e53cdc8ddca1632438ca6d6f7c735
vr Torn <varintron444@gmail.com>
2023-10-30 11:44:34 +0700

    first app

---
+++ lib/person.dart
@@ -0,0 +1,13 @@
+class Person {
+  String? name = "Mart Zuckberg";
+  String? phone = "039521012";
+  bool? isMarried = true;
+  int? age = 53;
+
+  void displayinfo() {
+    print("person name is:name");
+    print("person phone is:phone");
+    print("person isMarried is:isMarried");
+    print("person age is:age");
+  }
+}
---
+++ test/dart_week3_test.dart
@@ -0,0 +1,14 @@
+class Person {
+  //
+  String? name;
+  String? phone;
+  bool? lsMarried;
+  int? age;
+
+  void diseplayinfeo(){
+  print(name);
+  print(phone);
+  print(lsMarried);
+  print(age);
+ }
+}
